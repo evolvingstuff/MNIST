e4472b96f9166e1fcb8a6cb70a0307a09dad3f5a
Thomas Lahore <tom.lahore@gmail.com>
2013-01-10 17:21:46 -0800

    Some minor changes. Also catches error if attempt to load nn without files

--- src/com/evolvingstuff/App.java
+++ src/com/evolvingstuff/App.java
@@ -10,39 +10,39 @@ import com.evolvingstuff.task.MNIST;
 public class App {
 	public static void main(String[] args) throws Exception {
 		
-		Random r = new Random(54324);
-		
-		boolean doLoad = false;
-		boolean doSave = true;
-		
-		MNIST task = new MNIST();
-
 		System.out.println("MNIST");
-
-		int epoches = 500;
-		int hidden_per_layer = 6000;
-		Neuron neuron = new RectifiedLinearNeuron(0.01);
-		double init_weight_range = 0.2;
-		double learning_rate = 0.3;
 		
-		String path = "data/";
+		final Random r = new Random(5532122);//54324
+		final MNIST task = new MNIST();
+		final int epoches = 500;
+		final int hidden_per_layer = 6000;
+		final Neuron neuron = new RectifiedLinearNeuron(0.01);
+		final double init_weight_range = 0.2;
+		final double learning_rate = 0.3;
+		final FlatJumpNetwork agent = new FlatJumpNetwork(r, task.GetObservationDimension(), task.GetActionDimension(), hidden_per_layer, neuron, init_weight_range, learning_rate);
+		final String path = "data/";
+		final boolean load_initially = false;
+		final boolean save_on_improvement = true;
 		
-		FlatJumpNetwork agent = new FlatJumpNetwork(r, task.GetObservationDimension(), task.GetActionDimension(), hidden_per_layer, neuron, init_weight_range, learning_rate);
-		
-		if (doLoad) {
-			System.out.println("Loading previously saved agent.");
-			agent.Load(path);
+		if (load_initially) {
+			try {
+				System.out.println("Attempting to load previously saved agent from "+path+".");
+				agent.Load(path);
+			}
+			catch (Exception e) {
+				System.out.println("Failed to load. Possible cause: "+path+" folder might be empty?");
+			}
 		}
 		
-		int low_at = 1;
+		int low_at = 0;
 		double low = Double.POSITIVE_INFINITY;
 		for (int t = 0; t < epoches; t++)
 		{
-			System.out.println("-----------------------------------------------------------------");
+			System.out.println("\n-----------------------------------------------------------------");
 			System.out.println("[epoch "+(t+1)+"]:");
 			
 			task.SetValidationMode(false);
-			double fit = task.EvaluateFitnessSupervised(agent);
+			task.EvaluateFitnessSupervised(agent);
 			task.SetValidationMode(true);
 			double validation = task.EvaluateFitnessSupervised(agent);
 
@@ -50,16 +50,13 @@ public class App {
 				low = 1 - validation;
 				low_at = t;
 				
-				if (doSave) {			
-					System.out.println("Lowest test error so far. Saving.");
+				if (save_on_improvement) {			
+					System.out.println("Lowest test error so far. Saving (this will overwrite contents of "+path+" folder.)");
 					agent.Save(path);
 				}
-				
 			}
 			System.out.println("lowest test error @ epoch " + (low_at+1));
 		}
-
 		System.out.println("\n\ndone.");
-
 	}
 }
